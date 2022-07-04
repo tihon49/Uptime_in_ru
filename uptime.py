5ebe952903cbe1bb7268032b19a58b95d1a90dbe
a.tihonov <a.tihonov@aerodisk.ru>
2022-07-04 10:50:22 +0300

    refactoring

--- uptime.py
+++ uptime.py
@@ -10,6 +10,32 @@ day_reg = r'[0-9]+ day'
 hour_reg = r'[0-9]+ hour'
 minute_reg = r'[0-9]+ minute'
 
+years_list = ['год', 'года', 'лет']
+months_list = ['месяц', 'месяца', 'месяцев']
+weeks_list = ['неделя', 'недели', 'недель']
+days_list = ['день', 'дня', 'дней']
+hours_list = ['час', 'часа', 'часов']
+minutes_list = ['минута', 'минуты', 'минут']
+
+
+def _shell(cmd):
+    """просто шелл"""
+    proc = subprocess.Popen(
+        cmd,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    try:
+        result = proc.communicate()
+        out = result[0].decode('utf-8')
+        error = result[1].decode('utf-8')
+    except Exception as e:
+        raise ValueError(f'Error: {e}')
+    if proc.returncode == 0:
+        return True, out
+    return False, error
+
 
 
 def uptime_to_web(value, words_list) -> str:
@@ -34,9 +60,13 @@ def create_string(reg_exp, uptime, words_list) -> str:
     """возвращает строку для добавления к строке вывода uptime на русском"""
     if re.search(reg_exp, uptime):
         data = re.search(reg_exp, uptime).group().split()[0]
-        return uptime_to_web(data, words_list) + " "
+        return uptime_to_web(data, words_list)
     return ""
 
+
+def _append_date_string_into_date_to_web_list(reg_exp, uptime, words_list, date_to_web_list):
+    if create_string(reg_exp, uptime, words_list):
+        date_to_web_list.append(create_string(reg_exp, uptime, words_list))
 
 
 def parse_uptime(uptime) -> str:
@@ -46,41 +76,17 @@ def parse_uptime(uptime) -> str:
     парсит uptime и выводит его на русском в правильном склонении
     возвращает полную строку uptime'а
     """
-    date_to_web = ''
+    date_to_web = []
 
-    years_list = ['год', 'года', 'лет']
-    months_list = ['месяц', 'месяца', 'месяцев']
-    weeks_list = ['неделя', 'недели', 'недель']
-    days_list = ['день', 'дня', 'дней']
-    hours_list = ['час', 'часа', 'часов']
-    minutes_list = ['минута', 'минуты', 'минут']
+    # добавляем в date_to_web строки согласно регулярным выражениям
+    _append_date_string_into_date_to_web_list(year_reg, uptime, years_list, date_to_web)
+    _append_date_string_into_date_to_web_list(month_reg, uptime, months_list, date_to_web)
+    _append_date_string_into_date_to_web_list(week_reg, uptime, weeks_list, date_to_web)
+    _append_date_string_into_date_to_web_list(day_reg, uptime, days_list, date_to_web)
+    _append_date_string_into_date_to_web_list(hour_reg, uptime, hours_list, date_to_web)
+    _append_date_string_into_date_to_web_list(minute_reg, uptime, minutes_list, date_to_web)
 
-    date_to_web += create_string(year_reg, uptime, years_list)
-    date_to_web += create_string(month_reg, uptime, months_list)
-    date_to_web += create_string(week_reg, uptime, weeks_list)
-    date_to_web += create_string(day_reg, uptime, days_list)
-    date_to_web += create_string(hour_reg, uptime, hours_list)
-    date_to_web += create_string(minute_reg, uptime, minutes_list)
-
-    return date_to_web.strip()
-
-
-def _shell(cmd):
-    proc = subprocess.Popen(
-        cmd,
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
-    try:
-        result = proc.communicate()
-        out = result[0].decode('utf-8')
-        error = result[1].decode('utf-8')
-    except Exception as e:
-        raise ValueError(f'Error: {e}')
-    if proc.returncode == 0:
-        return True, out
-    return False, error
+    return ", ".join(date_to_web)
 
 
 def days_count_from_uptime() -> int:
@@ -92,6 +98,8 @@ def days_count_from_uptime() -> int:
 
 
 
-# print(parse_uptime('up 4 years, 1 month, 3 weeks, 21 days, 15 hours, 45 minutes'))
-# Out: '4 года 1 месяц 3 недели 21 день 15 часов 45 минут'
+print(parse_uptime('up 1 year, 2 months, 3 weeks, 21 days, 15 hours, 45 minutes'))
+# Out: 1 год, 2 месяца, 3 недели, 21 день, 15 часов, 45 минут
 
+print(uptime_to_web(days_count_from_uptime(), ['день', 'дня', 'дней']))
+# Out: 15 дней
